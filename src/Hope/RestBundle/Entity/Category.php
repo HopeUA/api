f42ccb126a5c32995b94abb893598633ce65ab17
surgun9 <serge@luchianenco.com>
2014-07-18 15:17:52 +0300

    новая сущность Hope\RestBundle\Entity\Category

---
+++ src/Hope/RestBundle/Entity/Category.php
@@ -0,0 +1,58 @@
+<?php
+// src/Hope/RestBundle/Entity/Category.php
+namespace Hope\RestBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="category")
+ */
+class Category {
+
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $title;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Category
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+}
---
+++ src/Hope/RestBundle/HopeRestBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Hope\RestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class HopeRestBundle extends Bundle
+{
+}
